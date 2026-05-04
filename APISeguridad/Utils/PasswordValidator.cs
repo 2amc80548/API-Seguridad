41d70b1f37bd064cfdd4ed45ac9190919c80fb37
EdmundoZ12 <edmundozentenor12@gmail.com>
2026-05-03 22:19:14 -0400

    Logic for validating a secure password

---
+++ APISeguridad/Utils/PasswordValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace APISeguridad.Utils;
+
+public class PasswordValidator
+{
+    public bool IsValid(string password)
+    {
+        // Regla: Mínimo 12 caracteres y debe contener un símbolo [cite_end]
+        if (string.IsNullOrEmpty(password) || password.Length < 12)
+            return false;
+
+        bool hasSymbol = password.Any(ch => !char.IsLetterOrDigit(ch));
+        return hasSymbol;
+    }
+}
